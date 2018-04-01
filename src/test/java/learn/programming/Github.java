e82ef38bb627a4b69effadd6a86975c5ee884010
Michael <Michael@King.fios-router.home>
2018-04-01 17:31:13 -0400

    push test

--- src/test/java/learn/programming/Github.java
+++ src/test/java/learn/programming/Github.java
@@ -4,7 +4,8 @@ public class Github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Java coding is fun boiiii");
+		System.out.println("wubb wubb rick and morty");
 	}
 
 }
