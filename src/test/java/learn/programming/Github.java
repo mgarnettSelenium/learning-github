9d72bdb9b49eb2009ae20642b5b03d0f30ab82c9
Michael <Michael@King.fios-router.home>
2018-04-01 17:57:06 -0400

    adding 2 new java classes

--- src/test/java/learn/programming/Github.java
+++ src/test/java/learn/programming/Github.java
@@ -3,9 +3,8 @@ package learn.programming;
 public class Github {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Java coding is fun");
-		System.out.println("rick and morty");
+		System.out.println("Rick and Morty");
 	}
 
 }
