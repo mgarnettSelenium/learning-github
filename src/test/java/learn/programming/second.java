e85a6f5f87b6a8bc212e7c3590877519c9d51c5b
Michael <Michael@King.fios-router.home>
2018-04-10 21:49:27 -0400

    adding a class

--- src/test/java/learn/programming/second.java
+++ src/test/java/learn/programming/second.java
@@ -2,4 +2,45 @@ package learn.programming;
 
 public class second {
 	// This is a comment
+
+	public static void main(String[] args) throws Exception {
+		implimentingArrays();
+		Thread.sleep(500);
+		dubArrays();
+		Thread.sleep(1000);
+		warmUp();
+	}
+
+	public static void implimentingArrays() {
+		int cat[] = { 4, 109, 88, 13, 21, 17, 9 };
+		for (int a : cat) {
+			System.out.println(a);
+		}
+	}
+
+	public static void dubArrays() {
+		int cat[][] = { { 15, 22, 33, 44 }, { 50, 60 } };
+
+		for (int[] x1 : cat) {
+			for (int b : x1) {
+				System.err.println(b);
+			}
+		}
+	}
+	
+	public static void  warmUp() {
+		int x = 0;
+		
+		do {
+			x++; //1, 4, 8
+			System.out.println(x); //1, 4, 8
+			if(++x < 5) { //2, 5, 9
+				continue;
+			}
+			x++; // 2, 6, 10
+			System.out.println(x); //2, 6, 10
+		}while(x++ < 10); //3, 7, 10
+	}
+	
+	
 }
